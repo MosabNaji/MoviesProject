69e7959e4b4a3bba89541af9da9e176df6ac498b
Mosab Naji <MosabNaji2002@Gmail.com>
2023-09-12 21:47:29 +0300

    Add project files.

---
+++ DotNetCore5CRUD/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore5CRUD.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ DotNetCore5CRUD/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿using DotNetCore5CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCore5CRUD.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required field"), StringLength(250),Display(Name = "Title")]
+        public string Title { get; set; }
+        [ Display(Name = "Year")]
+        public int Year { get; set; }
+
+        [Range(1, 10), Display(Name = "Rate")]
+        public double Rate { get; set; }
+
+        [Required(ErrorMessage = "Required field"), StringLength(2500), Display(Name = "Storeline")]
+        public string Storeline { get; set; }
+
+        [Display(Name = "Select poster...")]
+        public byte[] Poster { get; set; }
+
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
